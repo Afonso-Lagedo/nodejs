b51b60f95e35b7be13ae778ae6600d2b41569f80
Afonso-devf <afonso.devf@gmail.com>
2024-10-15 10:10:55 -0300

    feat: new type handlebars

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const app = express();
-const handlebars = require("express-handlebars");
+const { create } = require("express-handlebars");
 const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize("teste", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
 
 //Config
 
 //Template Engine
+/*
+OLD FORM
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");*/
+
+//NEW FORM
+const hbs = create({ defaultLayout: "main" });
+
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
+//Conexão com o banco de dados
+const sequelize = new Sequelize("teste", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 //finally
 app.listen(3031, function () {
